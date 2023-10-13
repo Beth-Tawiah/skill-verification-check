53b7e395d95b54131bda6e1bd556b71095dc7fac
BethTawiah <elizabeth.tawiah@amalitech.org>
2023-10-13 14:07:49 +0000

    user dashboard

--- skill-verification-frontend/src/pages/Login.jsx
+++ skill-verification-frontend/src/pages/Login.jsx
@@ -1,19 +1,27 @@
-// Login.js
-import { useMetaMask } from "metamask-react";
+import React from 'react';
+import { useMetaMask } from 'metamask-react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate from React Router
 import './Login.css';
 
 function Login() {
-    const { connect,status } = useMetaMask();
+  const { connect, status } = useMetaMask();
+  const navigate = useNavigate(); // Get the navigate function from React Router
 
-    const handleLogin = () => {
-        connect();
+  const handleLogin = async () => {
+    await connect();
+    if (status === 'connected') {
+      // Navigate to the UserDashboard page if connected
+      navigate('/UserDashboard');
     }
+  };
 
-    return (
-        <div>
-            <button className="login-button" onClick={handleLogin}>Connect Wallet</button>
-        </div>
-    );
+  return (
+    <div>
+      <button className="login-button" onClick={handleLogin}>
+        Log In
+      </button>
+    </div>
+  );
 }
 
 export default Login;
